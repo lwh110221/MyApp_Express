28d31e08e8ad0452144ce3e5d7159892c8048979
lwh110221 <2285813721@qq.com>
2025-01-12 15:01:17 +0800

    refactor(app): 重新组织日志中间件调用位置，确保一致性；docs: 更新新闻相关图片上传和处理说明，添加相关文章获取规则

--- app.js
+++ app.js
@@ -81,9 +81,6 @@ app.use(cors({
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1mb' }));
 
-// 使用日志中间件
-app.use(loggerMiddleware);
-
 // 统一响应处理
 app.use((req, res, next) => {
   res.success = (data, message) => {
@@ -123,6 +120,9 @@ app.use((req, res) => {
 
 // 错误处理中间件
 app.use(errorHandler);
+
+// 使用日志中间件
+app.use(loggerMiddleware);
 
 // 优雅关闭
 process.on('SIGTERM', () => {
